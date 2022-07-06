02b0770af971ecc680e7409506a8427844f5b433
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-07-06 16:21:54 +0000

    feat: Add six different questions to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,128 @@
-const questions = []
+const questions = [
+    {
+        question: "What is the seat of house Tully?",
+        answers: [{
+                text: 'Winterfell',
+                correct: false
+            },
+            {
+                text: 'Riverrun',
+                correct: true
+            },
+            {
+                text: 'Casterly Rock',
+                correct: false
+            },
+            {
+                text: 'The Eyrie',
+                correct: false
+            },
+
+        ]
+    },
+    {
+        question: "What was the sword given to Brienne named?",
+        answers: [{
+                text: 'Ice',
+                correct: false
+            },
+            {
+                text: 'Heartsbane',
+                correct: false
+            },
+            {
+                text: 'Oathkeeper',
+                correct: true
+            },
+            {
+                text: 'Lady Forlorn',
+                correct: false
+            },
+
+        ]
+    },
+    {
+        question: "What are the words of house Baratheon?",
+        answers: [{
+                text: 'Winter is Coming',
+                correct: false
+            },
+            {
+                text: 'Unbound, Unbent, Unbroken',
+                correct: false
+            },
+            {
+                text: 'We Do Not Sow',
+                correct: false
+            },
+            {
+                text: 'Ours Is The Fury',
+                correct: true
+            },
+
+        ]
+    },
+    {
+        question: "What kingdom does house Greyjoy rule over?",
+        answers: [{
+                text: 'The North',
+                correct: false
+            },
+            {
+                text: 'The Riverlands',
+                correct: false
+            },
+            {
+                text: 'Dorne',
+                correct: false
+            },
+            {
+                text: 'The Iron Islands',
+                correct: true
+            },
+
+        ]
+    },
+    {
+        question: "What was the monicker given to Lyonel Baratheon?",
+        answers: [{
+                text: 'The Mountain',
+                correct: false
+            },
+            {
+                text: 'The Laughing Storm',
+                correct: true
+            },
+            {
+                text: 'The Hound',
+                correct: false
+            },
+            {
+                text: 'The Kingslayer',
+                correct: false
+            },
+
+        ]
+    },
+    {
+        question: "Which kingsguard was called The Sword Of The Morning?",
+        answers: [{
+                text: 'Arthur Dayne',
+                correct: true
+            },
+            {
+                text: 'Gerold Hightower',
+                correct: true
+            },
+            {
+                text: 'Barristan Selmy',
+                correct: false
+            },
+            {
+                text: 'Lewyn Martell',
+                correct: false
+            },
+
+        ]
+    }
+]
